ed9127ba7210dee3d9419528431cd362cc445a94
Nikita Zenkov <cmaker.xtal@hotmail.com>
2020-11-20 17:00:23 +0600

    fix publish

--- AquilaNugetPublish.cmd
+++ AquilaNugetPublish.cmd
@@ -1,4 +1,4 @@
 REM Expects .NET Core SDK listed in global.json to be installed (other versions might work as well, but it was tested with this one)
-dotnet pack -c Release .\src\Compilers\Core\Portable\CodeAnalysis.csproj
+dotnet pack -c Release .\src\Compilers\Core\Portable\Microsoft.CodeAnalysis.csproj -p:StrongNameKeyId=Open
 
-dotnet nuget push .\Binaries\Release\Aquila.Microsoft.CodeAnalysis.0.0.5.nupkg -k 4267837e-9316-452e-9c3c-e26055f4cdba --source https://www.myget.org/F/aquila/api/v2/package
+dotnet nuget push .\artifacts\packages\Release\Shipping\Aquila.Microsoft.CodeAnalysis.3.7.0.nupkg -k 4267837e-9316-452e-9c3c-e26055f4cdba --source https://www.myget.org/F/aquila/api/v2/package
